5602c0fefb5966cf9b4d6f61690df503a375c0d4
AlejandroEcheverri <a.echeverrir@uniandes.edu.co>
2018-03-17 10:53:54 -0500

    finished tutorial until step 11

---
+++ server/main.js
@@ -0,0 +1,8 @@
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+ 
+import App from '../imports/ui/App.js';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
